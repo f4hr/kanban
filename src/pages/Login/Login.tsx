d5106722ea393fb187b04ae49162c24cefb10e89
Ilya Fakhrutdinov <bekamez@gmail.com>
2022-12-12 15:53:24 +0500

    chore(components): add responsive styles to Login and Signup forms

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { LoginForm } from '../../features/LoginForm';
 
 export function Login() {
-  return <LoginForm className="py-10 ml-auto mr-auto w-[420px]" />;
+  return <LoginForm className="py-10 ml-auto mr-auto sm:w-[420px]" />;
 }
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { SignupForm } from '../../features/SignupForm';
 
 export function Signup() {
-  return <SignupForm className="py-10 ml-auto mr-auto w-[420px]" />;
+  return <SignupForm className="py-10 ml-auto mr-auto sm:w-[420px]" />;
 }
